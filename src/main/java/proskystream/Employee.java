5b17b2afe3893a82803131cb61aa719e810033cf
mcfurry <mcfurry.only@gmail.com>
2023-05-21 12:21:29 +0300

    initial commit

---
+++ src/main/java/proskystream/Employee.java
@@ -0,0 +1,4 @@
+package proskystream;
+
+public class Employee {
+}
---
+++ src/test/java/proskystream/StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package proskystream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
